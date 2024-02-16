02d82c309de25ae7825dea97d8d2f7ab74e4463b
21501A05G3 <109433174+21501A05G3@users.noreply.github.com>
2024-02-16 15:43:37 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,41 @@
+const users = [
+
+{
+
+id: 1,
+
+name: 'Sk.Ismail',
+
+email: 'issuu@gmail.com',
+
+type: 'admin'
+
+},
+
+{
+
+id: 2,
+
+name: 'farzuu',
+
+email: 'farzuu@gmail.com',
+
+status: 'guest'
+
+},
+
+{
+
+id: 3,
+
+name: 'SakinabiSk',
+
+email: 'sakinabi55@gmail.com',
+
+status: 'admin'
+
+}
+
+];
+
+module.exports = users;
